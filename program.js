371ca10840564c64fb64ffa22dab44d3826efaf2
ikt-sf-bg-ac-rs <srbpro18@gmail.com>
2023-02-20 13:55:06 +0100

    v1 | 20.2.2023.

--- program.js
+++ program.js
@@ -16,7 +16,7 @@ glavniDiv.style.padding = '0.6em';
 //document.div - css
 function divStil(divNaziv){
 
-    divNaziv.style.width = '70%';
+    divNaziv.style.width = '90%';
     //divNaziv.style.border = '1px dashed black';
     //divNaziv.style.borderRadius = '10px';
     divNaziv.style.margin = '0 auto 0 auto';
@@ -84,7 +84,10 @@ tekstStil(txt3Dprikaz);
 
 
 function podrazumevaniPrikaz(){
-    divMapa.style.display = 'none';
+
+    //divMapa.style.display = 'none';
+    //divMapa3D.style.display = 'none';
+
 }
 
 
@@ -133,11 +136,24 @@ opcijePrikaza.forEach((item) => {
             
             podrazumevaniPrikaz();
             console.log('3d mapa');
-            obavestenje3Dmapa();
-            
+            //obavestenje3Dmapa();
+            prikaz3Dmape();
+            mapa3D();
+            /*
             setTimeout(function(){
                 window.location.reload(true);
             }, 3000);
+            */
+
+            if(li.textContent == "2D prikaz Beograda" || li.textContent == "Prikaz zemljotresa"){
+                
+                divMapa3D.style.display = 'none';
+                divMapa3D.style.visibility = 'hidden';
+
+                prikaz2Dmape();
+                mapa2D();
+
+            }
 
         } else if(li.textContent == "Prikaz zemljotresa"){
 
@@ -158,6 +174,12 @@ divMapa.id = 'divMapaI';
 
 divStil(divMapa);
 
+let divMapa3D = document.createElement('div');
+glavniDiv.appendChild(divMapa3D);
+divMapa3D.id = 'divMapa3Di';
+
+divStil(divMapa3D);
+
 /*
 divMapa.style.width = '90%';
 divMapa.style.height = '37em';
@@ -166,10 +188,33 @@ divMapa.style.borderRadius = '20px';
 */
 function prikaz2Dmape(){
 
+    divMapa3D.style.visibility = 'hidden';
+    divMapa3D.style.height = '0px';
+    //divMapa.style.display = 'block';
+    //divMapaI.style.display = 'block';
+    divMapa.style.visibility = 'visible';
+
     divMapa.style.width = '90%';
     divMapa.style.height = '37em';
     divMapa.style.border = '2px dashed rgb(114,160,193)';
     divMapa.style.borderRadius = '20px';
+
+    //divMapa3D.style.display = 'none';
+
+}
+
+function prikaz3Dmape(){
+
+    
+    divMapa.style.visibility = 'visible';
+    divMapa.style.height = '0px';
+    divMapa.style.border = '0px none rgba(0, 0, 0, 0)';
+    divMapa3D.style.visibility = 'visible';
+
+    divMapa3D.style.width = '90%';
+    divMapa3D.style.height = '37em';
+    divMapa3D.style.border = '2px dashed black';
+    divMapa3D.style.borderRadius = '20px';
 
 }
 
@@ -260,3 +305,43 @@ function obavestenje3Dmapa(){
 
 }
 
+function mapa3D(){
+
+    require([
+
+        "esri/config",
+        "esri/Map",
+        "esri/views/SceneView",
+        
+
+    ], (esriConfig, Map, SceneView)=> {
+
+        esriConfig.apiKey = 'AAPK2ea63d879cc54628aca165f612c7c18eHL3sBlfMalg4D01a4F_HmA6xLn8REJCoLB0Musdq9kkarcQVu0cH8M9BFw8CjFmD';
+
+        const map = new Map({
+
+            basemap: 'arcgis-topographic',
+            ground: 'world-elevation'
+
+        });
+
+        const view = new SceneView({
+
+            map: map,
+            container: 'divMapa3Di',
+            camera: {
+                position: {
+
+                    x: 20.457273, //longitude - geo. dužina
+                    y: 44.797197, //latitude - geo. širina
+                    z: 1000 //Nadmorska visina
+
+                }, 
+                tilt: 80
+            }
+
+        });
+
+    });
+
+}
